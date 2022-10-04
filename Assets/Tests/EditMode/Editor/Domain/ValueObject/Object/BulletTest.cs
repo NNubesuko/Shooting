855cc24f1f9c421ea2a98b321a3a3035f1e01ea2
KaonashiNubesuko <yoshi37770129@au.com>
2022-10-04 13:44:39 +0900

    BulletTestとBulletのテストと実装（仮）

--- Assets/Tests/EditMode/Editor/Domain/ValueObject/Object/BulletTest.cs
+++ Assets/Tests/EditMode/Editor/Domain/ValueObject/Object/BulletTest.cs
@@ -13,7 +13,7 @@ namespace Tests {
 
         [Test]
         [Description("通常の弾丸を選択した場合に、通常の弾丸が返却されること")]
-        public void ValidBullet() {
+        public void ValidNormalBullet() {
             BulletType responseBulletType = BulletType.Normal;
             BulletAP responseBulletAP = BulletAP.Of(10);
             BulletSpeed responseBulletSpeed = BulletSpeed.Of(25);
@@ -24,7 +24,21 @@ namespace Tests {
             Assert.That(bullet.AP, Is.EqualTo(responseBulletAP));
             Assert.That(bullet.Speed, Is.EqualTo(responseBulletSpeed));
         }
-    
+
+        [Test]
+        [Description("強化弾を選択した場合に、強化弾が返却されること")]
+        public void ValidHeadBullet() {
+            BulletType responseBulletType = BulletType.Head;
+            BulletAP responseBulletAP = BulletAP.Of(50);
+            BulletSpeed responseBulletSpeed = BulletSpeed.Of(10);
+
+            Bullet bullet = Bullet.Of(BulletType.Head);
+
+            Assert.That(bullet.Type, Is.EqualTo(responseBulletType));
+            Assert.That(bullet.AP, Is.EqualTo(responseBulletAP));
+            Assert.That(bullet.Speed, Is.EqualTo(responseBulletSpeed));
+        }
+
     }
 
 }
