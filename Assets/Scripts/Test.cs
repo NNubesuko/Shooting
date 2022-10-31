fc19b6c12d1465c4000fdef9979d78f3399579dc
YoshinobuKataoka <yoshi37770129@au.com>
2022-10-31 18:44:05 +0900

    Test変更

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Profiling;
 
 public class Test : MonoBehaviour {
 
     [SerializeField] private Text fpsText;
+    [SerializeField] private Text usedText;
+    [SerializeField] private Text unusedText;
+    [SerializeField] private Text totalText;
 
     private void Update() {
         float fps = 1f / Time.deltaTime;
-        fpsText.text = $"FPS: {fps}";
+        fpsText.text = fps.ToString("0.0") + " MB";
+
+        float used = ( Profiler.GetTotalAllocatedMemoryLong() >> 10 ) / 1024f;
+        float unused = ( Profiler.GetTotalUnusedReservedMemoryLong() >> 10 ) / 1024f;
+        float total = ( Profiler.GetTotalReservedMemoryLong() >> 10 ) / 1024f;
+
+        usedText.text = used.ToString("0.0") + " MB";
+        unusedText.text = unused.ToString("0.0") + " MB";
+        totalText.text = total.ToString("0.0") + " MB";
     }
 
 }
