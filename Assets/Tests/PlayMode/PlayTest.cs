740f82d49a8144570a3a2fca6a551f7582cfac14
YoshinobuKataoka_inUni <yoshi37770129@au.com>
2022-11-14 18:07:09 +0900

    PlayTest変更

--- Assets/Tests/PlayMode/PlayTest.cs
+++ Assets/Tests/PlayMode/PlayTest.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Systemk;
 using NUnit.Framework;
-using NUnit.Framework.Interfaces;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
 
-public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
+public class PlayTest {
 
     private GameObject playerObject;
     private Player playerScript;
+    private Vector2 lastPosition;
+    private Vector2 currentPosition;
 
     [OneTimeSetUp]
     public void InitializeTest() {
@@ -20,18 +21,8 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
         };
     }
 
-    public IEnumerator BeforeTest(ITest test) {
-        Debug.Log("Before Test");
-        yield break;
-    }
-
-    public IEnumerator AfterTest(ITest test) {
-        Debug.Log("After Test");
-        yield break;
-    }
-
     [UnityTest]
-    [Order(1)]
+    // [Order(1)]
     [Description("[正常] プレイヤーオブジェクトが存在している場合に、変数に格納されていること")]
     public IEnumerator PlayerObjectExists() {
         Assert.That(playerObject, Is.Not.Null);
@@ -39,7 +30,7 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
     }
 
     [UnityTest]
-    [Order(2)]
+    // [Order(2)]
     [Description("[正常] プレイヤースクリプトが存在している場合に、変数に格納されていること")]
     public IEnumerator PlayerScriptExists() {
         Assert.That(playerScript, Is.Not.Null);
@@ -47,7 +38,7 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
     }
 
     [UnityTest]
-    [Order(3)]
+    // [Order(3)]
     [Description("[正常] ゲーム上のプレイヤーのステータスが、インスペクター上の数値と一致すること")]
     public IEnumerator PlayerStatusAndInspectorValueMatch() {
         PlayerHP hp = PlayerHP.Of(100);
@@ -69,7 +60,7 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
     }
 
     [UnityTest]
-    [Order(4)]
+    // [Order(4)]
     [Description("[正常] スコアが追加された場合に、追加された分のスコアが格納されていること")]
     public IEnumerator ProcessingAddScoreNormal() {
         PlayerScore responseFirstScore = PlayerScore.Of(10);
@@ -92,7 +83,7 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
     }
     
     [UnityTest]
-    [Order(5)]
+    // [Order(5)]
     [Description("[正常] ダメージを受けた場合に、ダメージを受けた分体力が減少していること")]
     public IEnumerator ProcessingDamageNormal() {
         PlayerHP responseFirstHP = PlayerHP.Of(90);
@@ -115,71 +106,92 @@ public class PlayTest : NUnitAttribute, IOuterUnityTestAction {
     }
 
     [UnityTest]
-    [Order(6)]
+    // [Order(6)]
     [Description("[正常] 上に移動するキーを入力した場合に、上に移動していること")]
     public IEnumerator UpKeyToMovePlayer() {
-        Vector2 lastPosition = default;
         yield return new WaitUntil(() => {
             lastPosition = playerObject.transform.position;
             return Inputk.GetAxis() == Vector2.up;
         });
+        // 入力が来てから、1フレーム経ったときに移動判定が来るため、1フレーム待つ
         yield return null;
-        // 入力が来てから、1フレーム経ったときに移動判定が来るため、1フレーム待ち判定を行う
-        Vector2 currentPosition = playerObject.transform.position;
-        Assert.That(currentPosition.y, Is.GreaterThan(lastPosition.y));
+        currentPosition = playerObject.transform.position;
+        // * 差分の正規化された値が上方向のものであれば、上方向に移動していると判定できるため差分を取得する
+        Vector2 diffPosition = currentPosition - lastPosition;
+
+        Assert.That(diffPosition.normalized, Is.EqualTo(Vector2.up));
     }
 
     [UnityTest]
-    [Order(7)]
+    // [Order(7)]
     [Description("[正常] 下に移動するキーを入力した場合に、下に移動していること")]
     public IEnumerator DownKeyToMovePlayer() {
-        Vector2 lastPosition = default;
         yield return new WaitUntil(() => {
             lastPosition = playerObject.transform.position;
             return Inputk.GetAxis() == Vector2.down;
         });
+        // 入力が来てから、1フレーム経ったときに移動判定が来るため、1フレーム待つ
         yield return null;
-        Vector2 currentPosition = playerObject.transform.position;
-        Assert.That(currentPosition.y, Is.LessThan(lastPosition.y));
+        currentPosition = playerObject.transform.position;
+        // * 差分の正規化された値が上方向のものであれば、上方向に移動していると判定できるため差分を取得する
+        Vector2 diffPosition = currentPosition - lastPosition;
+
+        Assert.That(diffPosition.normalized, Is.EqualTo(Vector2.down));
     }
 
     [UnityTest]
-    [Order(8)]
+    // [Order(8)]
     [Description("[正常] 左に移動するキーを入力した場合に、左に移動していること")]
     public IEnumerator LeftKeyToMovePlayer() {
-        Vector2 lastPosition = default;
         yield return new WaitUntil(() => {
             lastPosition = playerObject.transform.position;
             return Inputk.GetAxis() == Vector2.left;
         });
+        // 入力が来てから、1フレーム経ったときに移動判定が来るため、1フレーム待つ
         yield return null;
-        Vector2 currentPosition = playerObject.transform.position;
-        Assert.That(currentPosition.x, Is.LessThan(lastPosition.x));
+        currentPosition = playerObject.transform.position;
+        // * 差分の正規化された値が上方向のものであれば、上方向に移動していると判定できるため差分を取得する
+        Vector2 diffPosition = currentPosition - lastPosition;
+
+        Assert.That(diffPosition.normalized, Is.EqualTo(Vector2.left));
     }
 
     [UnityTest]
-    [Order(9)]
+    // [Order(9)]
     [Description("[正常] 右に移動するキーを入力した場合に、右に移動していること")]
     public IEnumerator RihgtKeyToMovePlayer() {
-        Vector2 lastPosition = default;
-        yield return new WaitUntil(() => {
-            lastPosition = playerObject.transform.position;
-            return Inputk.GetAxis() == Vector2.right;
-        });
+        // yield return new WaitUntil(Handler(playerObject, Vector2.right));
+        // yield return new WaitUntil(() => {
+        //     lastPosition = playerObject.transform.position;
+        //     return Inputk.GetAxis() == Vector2.right;
+        // });
+        // 入力が来てから、1フレーム経ったときに移動判定が来るため、1フレーム待つ
         yield return null;
-        Vector2 currentPosition = playerObject.transform.position;
-        Assert.That(currentPosition.x, Is.GreaterThan(lastPosition.x));
-    }
-    
-    [UnityTest]
-    [Order(100)]
-    [Description("[正常] 体力が0になった場合に、プレイヤーオブジェクトが非アクティブ状態になっていること")]
-    public IEnumerator PlayerObjectInactiveWhenPlayerHPIsZero() {
-        playerScript.Damage(PlayerHP.MAX);
+        currentPosition = playerObject.transform.position;
+        // * 差分の正規化された値が上方向のものであれば、上方向に移動していると判定できるため差分を取得する
+        Vector2 diffPosition = currentPosition - lastPosition;
 
-        yield return null;
-        // プレイヤーの体力が0になってから、1フレーム経ったときに死亡判定が来るため、1フレーム待ち判定を行う
-        Assert.That(GameObject.Find("Player"), Is.Null);
+        Assert.That(diffPosition.normalized, Is.EqualTo(Vector2.right));
     }
+
+    // private System.Func<bool> Handler(GameObject gameObject, Vector2 direction) {
+    //     lastPosition = gameObject.transform.position;
+    //     return Inputk.GetAxis() == direction;
+    // }
+
+    // private void Handler(System.Action method, Vector2 direction) {
+
+    // }
+    
+    // [UnityTest]
+    // [Order(100)]
+    // [Description("[正常] 体力が0になった場合に、プレイヤーオブジェクトが非アクティブ状態になっていること")]
+    // public IEnumerator PlayerObjectInactiveWhenPlayerHPIsZero() {
+    //     playerScript.Damage(PlayerHP.MAX);
+
+    //     yield return null;
+    //     // プレイヤーの体力が0になってから、1フレーム経ったときに死亡判定が来るため、1フレーム待ち判定を行う
+    //     Assert.That(GameObject.Find("Player"), Is.Null);
+    // }
 
 }
